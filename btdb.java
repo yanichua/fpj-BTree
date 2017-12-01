c4b06e4b3811458680fb6ee4fc46582b02666011
perkykooky <cojulieanne@gmail.com>
2017-12-01 11:01:46 +0800

    I/O and searchnode
    - modified input structure to lessen method parameters
    - completed switch statements
    - proposing a separate method for searching the right node and for inserting

--- btdb.java
+++ btdb.java
@@ -25,99 +25,79 @@ public class btdb{
 	public static int[] keyArray; //Keys
 	public static ArrayList<int[]> Records = new ArrayList<int[]>(); //Records of keys
 	public static ArrayList<String> Values = new ArrayList<String>(); //Records of Values
-		
+	
+	public static int root;
+	
+	static class Input{
+		String command;
+		String value="";
+		int key;	
+		Input(String inp){
+			String[] explode = inp.split(" ");
+			this.command = explode[0];
+			if(explode.length>1) this.key = Integer.valueOf(Integer.parseInt(explode[1]));
+			if(explode.length>2) {
+				for (int i = 2; i < explode.length;++i){
+					if(i>2) this.value+=" ";
+					this.value += explode[i];
+				}	
+			}
+		}
+	}
 	public static void main(String[] args)throws IOException {
 		//error handling for if file exist
-
 		//====================================================
 		File_bt = args[0];
 		File_values = args[1];		
 		
 		//Initialize
 		createNew();
+		root=0;
 		keyArray= Records.get(0);
 		Scanner sc = new Scanner(System.in);
 		System.out.print(">");
+		
 		while (sc.hasNext())
 		{	
 			//Input
-			String input = sc.nextLine();				
-			String[] dictionary = input.split(" ");	
-			String Command = dictionary[0];	
-			int keyInt = Integer.valueOf(dictionary[1]);
-			String valueString = "";
-			for (int i = 2; i < dictionary.length;++i){
-				if(i == 2){
-					valueString = dictionary[i];
-				}
-				else{					
-					valueString = valueString + " " + dictionary[i];
-				}
-			}	
+			Input read = new Input(sc.nextLine());
+
 			//Commands
-			switch(Command)
+			switch(read.command)
 			{
 				case CMD_INSERT:
-					if(exist(keyInt)){
-						System.out.printf("< ERROR: %d already exists.\n", keyInt);
+					if(exist(read.key)){
+						System.out.printf("< ERROR: %d already exists.\n", read.key);
 						break;
 					}
-					insert(keyInt, valueString);
+					insert(read);
 					break;
 				case CMD_UPDATE:
-					if(!exist(keyInt)){
-						System.out.printf("< ERROR: %d does not exists.\n", keyInt);
+					if(!exist(read.key)){
+						System.out.printf("< ERROR: %d does not exists.\n", read.key);
 						break;
 					}
-					update(keyInt, valueString);					
+					update(read.key, read.value);					
 					break;
 				case CMD_SELECT:
-					if(!exist(keyInt)){
-						System.out.printf("< ERROR: %d does not exists.\n", keyInt);
+					if(!exist(read.key)){
+						System.out.printf("< ERROR: %d does not exists.\n", read.key);
 						break;
 					}
-					select(keyInt);
+					select(read.key);
 					break;
 				case CMD_DELETE:;
-					delete(keyInt, valueString);
+					delete(read.key, read.value);
 					break;
 				case CMD_EXIT:
+					System.exit(0);
+					break;
+				default:
+					System.out.println("ERROR: invalid command");
 					break;
 			}			
 			System.out.print(">");
 		}
-	}
-		
-	public static void insert(int key, String value)throws IOException{				
-		if (keyArray[length - 3] != -1){ 	
-			System.out.printf("< %s !!\n", "FULL");
-			//split(key);
-		}		
-		for(int i = 2; i < length; i = i+3){
-			int keyTemp = keyArray[i];
-			if(keyTemp == -1){ 							//if empty space
-				keyArray[i] = key; 						//insert key
-				keyArray[i+1] = value_recordCount; 		//insert offset of value
-				break;
-			}
-			else{
-				if (keyTemp > key){						
-					for(int j =  length - 6; j >= i; j = j-3){
-						if (keyArray[j] != -1){							
-							keyArray[j+3] = keyArray[j];		//insert key
-							keyArray[j+3+1] = keyArray[j+1];	//insert offset of value												
-						}
-					}
-					keyArray[i] = key; 						//insert key
-					keyArray[i+1] = value_recordCount; 		//insert offset of value
-					break;
-				}					
-			}
-		}
-		Values.add(value);  //add value to value array	
-		write(value);
-		value_recordCount += 1;			
-		System.out.printf("< %d inserted.\n", key);
 	}
 	
 	public static void createNew() {
@@ -125,6 +105,48 @@ public class btdb{
 		Arrays.fill(newRecords, new Integer(-1));
 		Records.add(newRecords);
 	}
+	
+	public static void searchNode(Input read, int focus, int index) {
+		//focus starts with root
+		//if(not full, insert)
+		//if(full, focus++)
+			//(if focus++ > number of nodes, createnew)
+			//(else move to next node)
+	}
+		
+	public static void insert(Input read)throws IOException{				
+		if (keyArray[length - 3] != -1){ 	
+			System.out.printf("< %s !!\n", "FULL");
+			//split(key);
+		}		
+		for(int i = 2; i < length; i = i+3){
+			int keyTemp = keyArray[i];
+			if(keyTemp == -1){ 							//if empty space
+				keyArray[i] = read.key; 						//insert key
+				keyArray[i+1] = value_recordCount; 		//insert offset of value
+				break;
+			}
+			else{
+				if (keyTemp > read.key){						
+					for(int j =  length - 6; j >= i; j = j-3){
+						if (keyArray[j] != -1){							
+							keyArray[j+3] = keyArray[j];		//insert key
+							keyArray[j+3+1] = keyArray[j+1];	//insert offset of value												
+						}
+					}
+					keyArray[i] = read.key; 						//insert key
+					keyArray[i+1] = value_recordCount; 		//insert offset of value
+					break;
+				}					
+			}
+		}
+		Values.add(read.value);  //add value to value array	
+		write(read.value);
+		value_recordCount += 1;			
+		System.out.printf("< %d inserted.\n", read.key);
+	}
+	
+	
 	
 	public static void split(){
 		
